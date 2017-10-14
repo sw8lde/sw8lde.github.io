48c32e3a72b4977996fac6cb9fcee6ebd3dc3363
sw8lde <sw8lde@gmail.com>
2017-10-14 09:59:55 -0500

    fixed links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,22 +20,22 @@ export class AppComponent {
       languages: 'Angular, AngularJS',
       desc: 'A web app to view statistics on Fantasy Premier League players. ' +
         'Made with Angular and an AngularJS branch.',
-      code: 'sw8lde/FPL-Analyzer',
+      code: '/FPL-Analyzer',
       link: 'https://sw8lde.github.io/FPL-Analyzer/'
     },
     {
       title: 'Battleship',
       languages: 'C',
       desc: 'The classic game of Battleship written in C for ECE 210. ' +
-        'Played using 2 Bucky Boards over Wi-Fi',
-      code: 'yyang0087/ece210_battleships/tree/master/ECE210-Battleship'
+        'Played using 2 Bucky Boards over Wi-Fi.',
+      code: '/Battleship'
     },
     {
       title: 'Zero Percent',
       languages: 'Android',
       desc: 'An Android app that sends a text with your location to selected ' +
         'contacts right before your phone dies.',
-      code: 'sw8lde/Zero-Percent'
+      code: '/Zero-Percent'
     },
     {
       title: 'GroupMe Bot',
@@ -43,7 +43,14 @@ export class AppComponent {
       desc: 'An example GroupMe bot written in Node.js. It can reply to ' +
         'specific messages, mention everyone in the group, post gifs based on ' +
         'search terms, and post statistics on the group or users.',
-      code: 'sw8lde/GroupMe-Bot'
+      code: '/GroupMe-Bot'
+    },
+    {
+      title: 'Ti-84 Programs',
+      languages: 'TI-BASIC',
+      desc: 'Calculator programs for TI-84 written in TIBASIC. Includes some ' +
+        'games, a triangle solver, and some other programs.',
+      code: '/Calulator-Programs'
     },
   ]
 }
